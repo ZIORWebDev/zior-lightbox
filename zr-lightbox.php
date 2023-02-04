5d4cf12b8cd1f0404c49c36204f32c2fd6a5ac4d
Rey Calantaol <reygcalantaol@gmail.com>
2023-02-04 16:28:19 +0800

    Update zr-lightbox.php

--- zr-lightbox.php
+++ zr-lightbox.php
@@ -6,7 +6,7 @@
  * Description: Convert all content images into lightbox.
  * Version: 0.1.0
  * Author:      ZIORWeb.Dev
- * Author URI:  https://github.com/ziorweb-dev
+ * Author URI:  https://github.com/ZIORWebDev
  * License:     GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  * Text Domain: zior-lightbox
